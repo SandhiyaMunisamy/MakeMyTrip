e064ab20ca504bbf42e41ddd0ccd790fecfc2f84
SandhiyaMunisamy <sandhiya.munisamy@atmecs.com>
2019-06-07 18:33:26 +0530

    modified hotel

--- MakeMyTripGIT/src/com/atmecs/MakeMyTripGIT/Hotel.java
+++ MakeMyTripGIT/src/com/atmecs/MakeMyTripGIT/Hotel.java
@@ -3,6 +3,7 @@ package com.atmecs.MakeMyTripGIT;
 public class Hotel {
 	public static void main(String[] args) {
 		System.out.println("Hotel Booking");
+System.out.println("modified hotel");
 	}
 
 }
