6bd471f7e54715c046d29f7357d7c20b89212948
SandhiyaMunisamy <sandhiya.munisamy@atmecs.com>
2019-06-19 17:29:29 +0530

    Exception handling commited

---
+++ Exception/src/main/java/com/atmecs/ExceptionHandling/Exception/PasswordCorrectionException.java
@@ -0,0 +1,16 @@
+package com.atmecs.ExceptionHandling.Exception;
+
+
+	public class PasswordCorrectionException extends Exception {
+
+		
+
+		public PasswordCorrectionException(String message) {
+
+			super(message);
+
+		
+
+		}
+
+}
